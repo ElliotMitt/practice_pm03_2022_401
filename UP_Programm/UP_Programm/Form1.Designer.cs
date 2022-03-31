49c3eb100c9a197f89959eec6ec3730b6f120d22
Quantrap <65012672+Quantrap@users.noreply.github.com>
2022-03-31 12:37:23 +0300

    Add files via upload
    Session 1

---
+++ UP_Programm/UP_Programm/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace UP_Programm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelLogos = new System.Windows.Forms.Panel();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelProduct = new System.Windows.Forms.Panel();
+            this.listViewProduct = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.panelMenu.SuspendLayout();
+            this.panelProduct.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogos
+            // 
+            this.panelLogos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLogos.BackgroundImage")));
+            this.panelLogos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLogos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogos.Location = new System.Drawing.Point(0, 0);
+            this.panelLogos.Name = "panelLogos";
+            this.panelLogos.Size = new System.Drawing.Size(200, 140);
+            this.panelLogos.TabIndex = 0;
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProduct.FlatAppearance.BorderSize = 0;
+            this.btnProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProduct.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnProduct.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnProduct.Location = new System.Drawing.Point(0, 140);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(200, 54);
+            this.btnProduct.TabIndex = 1;
+            this.btnProduct.Text = "Товары";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrders.FlatAppearance.BorderSize = 0;
+            this.btnOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrders.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOrders.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnOrders.Location = new System.Drawing.Point(0, 194);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(200, 54);
+            this.btnOrders.TabIndex = 2;
+            this.btnOrders.Text = "Заказы";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomers.FlatAppearance.BorderSize = 0;
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCustomers.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnCustomers.Location = new System.Drawing.Point(0, 248);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(200, 54);
+            this.btnCustomers.TabIndex = 3;
+            this.btnCustomers.Text = "Клиенты";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployees.FlatAppearance.BorderSize = 0;
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEmployees.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnEmployees.Location = new System.Drawing.Point(0, 302);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(200, 54);
+            this.btnEmployees.TabIndex = 4;
+            this.btnEmployees.Text = "Сотрудники";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnExit.Location = new System.Drawing.Point(0, 457);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 54);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.SeaGreen;
+            this.panelMenu.Controls.Add(this.btnExit);
+            this.panelMenu.Controls.Add(this.btnEmployees);
+            this.panelMenu.Controls.Add(this.btnCustomers);
+            this.panelMenu.Controls.Add(this.btnOrders);
+            this.panelMenu.Controls.Add(this.btnProduct);
+            this.panelMenu.Controls.Add(this.panelLogos);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 511);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelProduct
+            // 
+            this.panelProduct.Controls.Add(this.listViewProduct);
+            this.panelProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelProduct.Location = new System.Drawing.Point(200, 0);
+            this.panelProduct.Name = "panelProduct";
+            this.panelProduct.Size = new System.Drawing.Size(584, 511);
+            this.panelProduct.TabIndex = 1;
+            // 
+            // listViewProduct
+            // 
+            this.listViewProduct.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listViewProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listViewProduct.FullRowSelect = true;
+            this.listViewProduct.GridLines = true;
+            this.listViewProduct.HideSelection = false;
+            this.listViewProduct.Location = new System.Drawing.Point(0, 0);
+            this.listViewProduct.MultiSelect = false;
+            this.listViewProduct.Name = "listViewProduct";
+            this.listViewProduct.Size = new System.Drawing.Size(584, 219);
+            this.listViewProduct.TabIndex = 6;
+            this.listViewProduct.UseCompatibleStateImageBehavior = false;
+            this.listViewProduct.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Цена";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Количество";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Оценка";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Категория";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Описание";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 511);
+            this.Controls.Add(this.panelProduct);
+            this.Controls.Add(this.panelMenu);
+            this.MinimumSize = new System.Drawing.Size(800, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelProduct.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panelLogos;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelProduct;
+        private System.Windows.Forms.ListView listViewProduct;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ UP_Programm/UP_Programm/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UP_Programm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            startDesign();
+            panel3.Visible = true;
+            panelProduct.Visible = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+            panelProduct.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void startDesign()
+        {
+            
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
