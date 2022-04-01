f8c6a0c7d9f6eeac6ef08c89d8da4e6dfdb97873
Quantrap <65012672+Quantrap@users.noreply.github.com>
2022-04-01 12:19:17 +0300

    Session 2

---
+++ UP_Programm/UP_Programm/FormMenu.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace UP_Programm
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
+            this.panelLogos = new System.Windows.Forms.Panel();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.btnGroupProduct = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogos
+            // 
+            this.panelLogos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLogos.BackgroundImage")));
+            this.panelLogos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLogos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogos.Location = new System.Drawing.Point(0, 0);
+            this.panelLogos.Name = "panelLogos";
+            this.panelLogos.Size = new System.Drawing.Size(143, 114);
+            this.panelLogos.TabIndex = 0;
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProduct.FlatAppearance.BorderSize = 0;
+            this.btnProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProduct.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnProduct.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnProduct.Location = new System.Drawing.Point(0, 114);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(143, 54);
+            this.btnProduct.TabIndex = 1;
+            this.btnProduct.Text = "Товары";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrders.FlatAppearance.BorderSize = 0;
+            this.btnOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrders.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOrders.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnOrders.Location = new System.Drawing.Point(0, 168);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(143, 54);
+            this.btnOrders.TabIndex = 2;
+            this.btnOrders.Text = "Заказы";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomers.FlatAppearance.BorderSize = 0;
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCustomers.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnCustomers.Location = new System.Drawing.Point(0, 222);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(143, 54);
+            this.btnCustomers.TabIndex = 3;
+            this.btnCustomers.Text = "Клиенты";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployees.FlatAppearance.BorderSize = 0;
+            this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployees.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEmployees.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnEmployees.Location = new System.Drawing.Point(0, 276);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(143, 54);
+            this.btnEmployees.TabIndex = 4;
+            this.btnEmployees.Text = "Сотрудники";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnExit.Location = new System.Drawing.Point(0, 507);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(143, 54);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.SeaGreen;
+            this.panelMenu.Controls.Add(this.buttonSort);
+            this.panelMenu.Controls.Add(this.btnGroupProduct);
+            this.panelMenu.Controls.Add(this.btnExit);
+            this.panelMenu.Controls.Add(this.btnEmployees);
+            this.panelMenu.Controls.Add(this.btnCustomers);
+            this.panelMenu.Controls.Add(this.btnOrders);
+            this.panelMenu.Controls.Add(this.btnProduct);
+            this.panelMenu.Controls.Add(this.panelLogos);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(143, 561);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonSort.FlatAppearance.BorderSize = 0;
+            this.buttonSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSort.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSort.ForeColor = System.Drawing.Color.GhostWhite;
+            this.buttonSort.Location = new System.Drawing.Point(0, 384);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(143, 54);
+            this.buttonSort.TabIndex = 7;
+            this.buttonSort.Text = "Сортировка";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // btnGroupProduct
+            // 
+            this.btnGroupProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnGroupProduct.FlatAppearance.BorderSize = 0;
+            this.btnGroupProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGroupProduct.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGroupProduct.ForeColor = System.Drawing.Color.GhostWhite;
+            this.btnGroupProduct.Location = new System.Drawing.Point(0, 330);
+            this.btnGroupProduct.Name = "btnGroupProduct";
+            this.btnGroupProduct.Size = new System.Drawing.Size(143, 54);
+            this.btnGroupProduct.TabIndex = 6;
+            this.btnGroupProduct.Text = "Категории";
+            this.btnGroupProduct.UseVisualStyleBackColor = true;
+            this.btnGroupProduct.Click += new System.EventHandler(this.btnGroupProduct_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel1.Controls.Add(this.labelTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(143, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(841, 114);
+            this.panel1.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTitle.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.ForeColor = System.Drawing.Color.GhostWhite;
+            this.labelTitle.Location = new System.Drawing.Point(307, 48);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(224, 25);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Добро пожаловать!";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panelMain
+            // 
+            this.panelMain.AutoScroll = true;
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(143, 114);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(841, 447);
+            this.panelMain.TabIndex = 2;
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "FormMenu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panelLogos;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button btnGroupProduct;
+        private System.Windows.Forms.Button buttonSort;
+    }
+}
+
